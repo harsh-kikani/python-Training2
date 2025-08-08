a074fe353379fb9e3909c79a7572b3f2c02f9c88
harsh-kikani <harsh.kikani@exhibytesolution.com>
2025-08-08 16:20:44 +0530

    add dict.py

--- dict.py
+++ dict.py
@@ -1,4 +1,4 @@
-info = {'name':'rahul', 'age':21, 'eligible':True}
+'''info = {'name':'rahul', 'age':21, 'eligible':True}
 
 #print(info)
 #print (info.keys())
@@ -9,4 +9,14 @@ info = {'name':'rahul', 'age':21, 'eligible':True}
     
 print (info.items())
 for key, value in info.items():
-   print(f"{key}:{value}")
+   print(f"{key}:{value}")'''
+   
+emp1 = {122:45, 123:50, 124:60 , 125:70}
+emp2 = {222:78, 222:90}
+
+#emp1.update(emp2) # Merging two dictionaries
+#emp1.clear() 
+#emp1.pop(122) # Removing a key-value pair
+emp1.popitem() 
+del emp1[122] 
+print(emp1)
