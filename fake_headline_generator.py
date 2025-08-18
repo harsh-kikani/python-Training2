e24621087943efd6364961fea1a34cbdbd2ed417
harsh-kikani <harsh.kikani@exhibytesolution.com>
2025-08-18 12:59:39 +0530

    add list in fake_headline_genrerator.py.py

---
+++ fake_headline_generator.py
@@ -0,0 +1,32 @@
+import random
+
+subjects = [
+    "Shahrukh Khan",
+    "Virat Kohli",
+    "Nirmala Sitharaman",
+    "A Mumbai Cat",
+    "A Group of Monkeys",
+    "Prime Ministed Modi",
+    "Auto Rickshaw Driver from Delhi",
+]
+
+actions = [
+    "launches",
+    "cancels",
+    "dances with",
+    "eats",
+    "declares war on",
+    "orders",
+    "celebrates",
+]
+
+places_or_things = [
+    "at Red Fort",
+    "in Mumbai Local Train",
+    "a plote of samosa",
+    "inside parliament",
+    "at Ganga Ghat",
+    "during IPL Match",
+    "at India Gate",
+
+]
