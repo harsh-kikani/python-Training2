91032676f4b5fd19088814a99c76ef2b9cebc7dd
harsh-kikani <harsh.kikani@exhibytesolution.com>
2025-08-18 14:28:34 +0530

    add condition in fake_headline_generator.py

--- Hotelmenu.py
+++ Hotelmenu.py
@@ -13,6 +13,7 @@ print("Pizza: Rs100\nPasta: Rs70\nBurger: Rs60\nfrenkie: Rs80\nSalad: Rs40\nCoff
 order_total = 0
 
 item_1 = input("Enter the name of item you want to order = ")
+
 if item_1 in menu:
     order_total += menu[item_1]
     print(f"your item {item_1} has been added to your order")
@@ -21,6 +22,7 @@ else:
     print(f"ordered item {item_1} is not available yet!")
     
 another_order = input("Do you want to add another item? (yes/no) ")
+
 if another_order == "yes":
     item_2 = input("Enter the name of second item = ")
     if item_2 in menu:
--- fake_headline_generator.py
+++ fake_headline_generator.py
@@ -6,7 +6,7 @@ subjects = [
     "Nirmala Sitharaman",
     "A Mumbai Cat",
     "A Group of Monkeys",
-    "Prime Ministed Modi",
+    "Prime Minister Modi",
     "Auto Rickshaw Driver from Delhi",
 ]
 
@@ -23,10 +23,25 @@ actions = [
 places_or_things = [
     "at Red Fort",
     "in Mumbai Local Train",
-    "a plote of samosa",
+    "a plate of samosa",
     "inside parliament",
     "at Ganga Ghat",
     "during IPL Match",
     "at India Gate",
 
 ]
+
+while True:
+    subject = random.choice(subjects)
+    action = random.choice(actions)
+    place_or_thing = random.choice(places_or_things)
+
+    headline = f" BREAKING NEWS: {subject} {action} {place_or_thing}"
+    print("\n" + headline)
+    
+    user_input = input("\nDo you want another headline? (yes/no)").strip().lower()
+    if user_input == "no":
+        break
+
+print("\nThank you for using the Fake Headline Generator. Have a fun day")
+ 
