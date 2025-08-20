0c14b5281fc92f5dfbe058f5cb6a180c764004af
harsh-kikani <harsh.kikani@exhibytesolution.com>
2025-08-20 16:00:46 +0530

    add class mathod in loops.py

--- pr_3_oop.py
+++ pr_3_oop.py
@@ -9,7 +9,7 @@ print(s1.name)
 del s1.name
 print(s1.name)'''
 
-
+#---------------------
 '''class Account:
     def __init__(self, acc_no, acc_pass):
         self.acc_no = acc_no
@@ -66,7 +66,7 @@ car1.start()'''
 #-----super() method---------
 
 
-class Car:
+'''class Car:
     def __init__(self, type):
         self.type = type
 
@@ -86,10 +86,76 @@ class ToyotaCar(Car):
 
 car1 = ToyotaCar("prius", "electric")
 print(car1.name)   
-print(car1.type)   
+print(car1.type)'''   
+
+#----------------------class method--------------------
+'''class Student:
+    def __init__(self, phy, chem, math):
+        self.phy = phy
+        self.chem = chem
+        self.math = math
+        self.percentage = str((self.phy + self.chem + self.math) / 3) + "%"
+        
+    def calcPercentage(self):
+        self.percentage = str((self.phy + self.chem + self.math) / 3) + "%"
+        
+stu1 = Student(98, 97, 99)
+print(stu1.percentage)
+
+stu1.phy = 88
+print(stu1.phy)
+stu1.calcPercentage()
+print(stu1.percentage)'''
+
+#-------------------------------------------------
+
+'''class Student:
+    def __init__(self, phy, chem, math):
+        self.phy = phy
+        self.chem = chem
+        self.math = math
+        
+    @property
+    def percentage(self):
+        return str((self.phy + self.chem + self.math) / 3) + "%"
+        
+stu1 = Student(98, 97, 99)
+print(stu1.percentage)
+
+stu1.phy = 88
+print(stu1.percentage)'''
+
 
         
+class Complex:
+    def __init__(self, real, img):
+        self.real = real
+        self.img = img
+    
+    def showNumber(self):
+        print(self.real,"i +", self.img,"j")
+        
+    def __add__(self, num2):
+        newReal = self.real + num2.real
+        newImg = self.img + num2.img 
+        return Complex(newReal, newImg)
+    
+    def __sub__(self, num2):
+        newReal = self.real - num2.real
+        newImg = self.img - num2.img 
+        return Complex(newReal, newImg)
+    
+num1 = Complex(1, 3)
+num1.showNumber()
 
+num2 = Complex(4, 6)
+num1.showNumber()
+
+num3 = num1 - num2
+num3.showNumber()
+    
+    
+        
 
 
         
