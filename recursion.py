741eb41eef94635323a7da37bee286e8cccbaaa8
harsh-kikani <harsh.kikani@exhibytesolution.com>
2025-08-25 12:03:04 +0530

    add code in recursion.py

--- recursion.py
+++ recursion.py
@@ -1,7 +1,27 @@
-def factorial(n):
+'''def factorial(n):
     if(n==1  or n==0):
         return 1
     return n * factorial(n-1)
 
 n = int(input("Enter a number: "))
-print(f"The factorial of {n} is {factorial(n)}")
+print(f"The factorial of {n} is {factorial(n)}")'''
+
+
+'''def show(n):
+    if(n == 0):
+        return
+    print(n)
+    show(n-1)
+    print("END")
+
+show(8)'''
+
+def print_list(list, idx=0):
+    if(idx == len(list)):
+        return
+    print(list[idx])
+    print_list(list, idx+1)
+    
+fruits = ["mango", "litchi", "apple", "banana"]
+
+print_list(fruits)
